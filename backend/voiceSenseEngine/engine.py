a3d463e959fd58fffc5c15161b1bbc5defc3c495
Daniel Ortega <daniel.ortega@cartesi.io>
2023-07-21 16:27:20 +0200

    feat(backend): voiceSense engine, using other dataset

---
+++ backend/voiceSenseEngine/engine.py
@@ -0,0 +1,21 @@
+from transformers import WhisperProcessor, WhisperForConditionalGeneration
+from datasets import load_dataset
+import soundfile as sf
+
+# load model and processor
+processor = WhisperProcessor.from_pretrained("openai/whisper-large-v2")
+model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-large-v2")
+model.config.forced_decoder_ids = None
+
+# load dummy dataset and read audio files
+ds = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation")
+sample = ds[0]["audio"]
+input_features = processor(sample["array"], sampling_rate=sample["sampling_rate"], return_tensors="pt").input_features 
+
+
+# generate token ids
+predicted_ids = model.generate(input_features)
+
+# decode token ids to text
+transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)
+print(transcription)
