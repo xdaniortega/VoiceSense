41213b0d260a4dbec4170ef9058fcde5d61ee70f
Daniel Ortega <daniel.ortega@cartesi.io>
2023-07-23 02:35:12 +0200

    feat: Perfect MVP

--- frontend/frontend-echo/src/Echoes.js
+++ frontend/frontend-echo/src/Echoes.js
@@ -2,6 +2,8 @@ import { ethers } from "ethers";
 import { useQuery, gql } from "@apollo/client";
 import { useToast } from "@chakra-ui/react";
 import React, { useState, useEffect } from "react";
+import QRCode from "qrcode.react";
+import Web3 from "web3";
 
 // GraphQL query to retrieve notices given a cursor
 const GET_NOTICES = gql`
@@ -24,6 +26,7 @@ const GET_NOTICES = gql`
         }
     }
 `;
+let qrCodeValue = ""; // Use an empty string as the default value
 
 // This component renders all the Notices produced by the Echo DApp.
 // The Echo Dapp uses notices to echo the Inputs it receives.
@@ -32,6 +35,7 @@ function EchoesList() {
     const toast = useToast();
     const [noticeEchoes, setNoticeEchoes] = useState([]);
     const [cursor, setCursor] = useState(null);
+    const [qrCodeValue, setQrCodeValue] = useState("");
 
     // Retrieve notices every 500 ms
     const { loading, error, data } = useQuery(GET_NOTICES, {
@@ -65,49 +69,82 @@ function EchoesList() {
         }
     });
 
-    // Check query result
+    //Check query result
     const length = data?.notices?.edges?.length;
+    let returnData = null;
     if (length) {
         // Update cursor so that next GraphQL poll retrieves only newer data
         setCursor(data.notices.pageInfo.endCursor);
+
+        // Render new echoes
+        const newEchoes = data?.notices?.edges?.map(({ node }) => {
+            // Render echo from notice
+            try {
+                const echo = ethers.utils.toUtf8String(node.payload);
+                console.log(`Detected new echo : ${echo}`);
+                // Define el patrón de regex para extraer el monto y la dirección del receptor
+                // Acepta tanto "Eth" como "Ethereum", y nombres de dominio con múltiples segmentos.
+                let pattern =
+                    /Send\s+(\d+(\.\d+)?)\s+(Eth|Ethereum|eth|ethereum|ETH)\s+to\s+([\w\.]+)/;
+
+                // Busca el patrón en el input_string
+                let match = echo.match(pattern);
+
+                if (match) {
+                    // Extrae el monto y la dirección del receptor desde los grupos capturados por el patrón de regex
+                    let amount = parseFloat(match[1]);
+                    let receiverAddress = match[4];
+
+                    returnData = JSON.stringify({ amount, receiverAddress });
+                    console.log(`Detected returnData : ${returnData}`);
+                } else {
+                    throw new Error("Input inválido");
+                }
+
+                if (returnData) {
+                    // Parse the returnData string back into an object
+                    const parsedReturnData = JSON.parse(returnData);
+
+                    // Create the transaction data object
+                    // Destructure the amount and receiver_address values
+                    const { amount, receiverAddress } = parsedReturnData;
+                    //const adr_ens = Web3.eth.ens.resolver(receiver_address);
+                    const valueInWei = ethers.utils.parseEther(
+                        amount.toString()
+                    );
+
+                    const transactionData = {
+                        to: receiverAddress,
+                        value: valueInWei.toString(), // Convertir a cadena porque value es un objeto BigNumber
+                        gas: 21000, // Establecer un límite de gas estándar para una transacción simple
+                        data: "0x", // No hay datos en una transacción simple
+                    };
+
+                    const data = JSON.stringify(transactionData);
+                    console.log("data", data);
+                    setQrCodeValue(data);
+                    console.log("qrCode:", qrCodeValue);
+                    return qrCodeValue ? (
+                        <QRCode value={qrCodeValue}></QRCode>
+                    ) : null;
+                }
+            } catch (error) {
+                toast({
+                    title: "Error",
+                    description: `${error.message}`,
+                    status: "error",
+                    duration: 5000,
+                    isClosable: true,
+                });
+            }
+        });
     }
-
-    // Render new echoes
-    const newEchoes = data?.notices?.edges?.map(({ node }) => {
-        // Render echo from notice
-        const echo = ethers.utils.toUtf8String(node.payload);
-        console.log(`Detected new echo : ${echo}`);
-
-        return (
-            <div key={`${node.input.index}-${node.index}`}>
-                <p>{echo}</p>
-            </div>
-        );
-    });
-
-    // Concat new echoes with previous ones
-    let ret = noticeEchoes;
-    if (newEchoes && newEchoes.length) {
-        // Add new rendered echoes to stored data
-        ret = noticeEchoes.concat(newEchoes);
-        setNoticeEchoes(ret);
-    }
-    return ret;
+    return qrCodeValue ? <QRCode value={qrCodeValue}></QRCode> : null;
 }
 
 function Echoes() {
     return (
         <div>
-            <img className="cave" src="/cave.jpg" alt="cave" />
-            <a
-                className="link"
-                href="https://www.freepik.com/free-vector/stone-cave-night-scene-isolated-white-background_9820224.htm#query=cave%20cartoon&position=31&from_view=search&track=sph"
-            >
-                Image by brgfx on Freepik
-            </a>
-            <label>
-                <p>Echoes...</p>
-            </label>
             <EchoesList />
         </div>
     );
